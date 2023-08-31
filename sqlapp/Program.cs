9a80e9fe76e55b7a1b2aafae5821953dd75c534b
nokorinotsubasa <alexsanderjuan13@gmail.com>
2023-08-31 15:35:57 -0300

    code secured

--- sqlapp/Program.cs
+++ sqlapp/Program.cs
@@ -1,23 +1,30 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using sqlapp.Services;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var keyVaultUri = new Uri("YOUR_KEY_VAULT_URI");
+var client = new SecretClient(keyVaultUri, new DefaultAzureCredential());
+
+KeyVaultSecret secret = client.GetSecret("SecretNameForConnectionString");
+string connectionString = secret.Value;
+
 builder.Services.AddRazorPages();
 
-string connectionString = "rediscache592027.redis.cache.windows.net:6380,password=q9ZQotxnmal1FafJd6rJdAoaCs8JIWYPZAzCaJeJ2s8=,ssl=True,abortConnect=False";
 var multiplexer = ConnectionMultiplexer.Connect(connectionString);
 builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
 builder.Services.AddTransient<IProductService, ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
